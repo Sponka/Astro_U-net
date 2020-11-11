6bcad3d05e21a2e5e1aa4b0b685a69740c5d0311
Antonia <37693099+Sponka@users.noreply.github.com>
2020-11-11 15:48:03 +0100

    Update down_data.py

--- code/down_data.py
+++ code/down_data.py
@@ -14,12 +14,13 @@ for f in filter_wide:
                                         instrument_name = ["WFC3/UVIS"], filters = [f])
 
     for url in obsTable['dataURL']:
-        if isinstance(url, type(obsTable[7]['dataURL'])):
+        if isinstance(url, type(obsTable[7]['dataURL'])):  #to have valid url, example: https://hla.stsci.edu/cgi-bin/getdata.cgi?dataset=hst_11426_21_wfc3_uvis_f606w_drz.fits
             if url[-5:] == str(".fits"):
                 file = "data/" + f + "/" + str(url[50:])
                 urllib.request.urlretrieve(url,file)
  
 #This code will download all fits images with filters F555W and F606W. 
+#Example of valid url https://hla.stsci.edu/cgi-bin/getdata.cgi?dataset=hst_11426_21_wfc3_uvis_f606w_drz.fits 
 #To filter used data the dataset.txt can be used
 #If image appears more times in dataset.txt it means that image was cut into multiple images
 
